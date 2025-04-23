f93586ec28aee75388c8d27329dc608b8a9d2284
Bassem Riachi <bassem.riachy@gmail.com>
2025-04-23 16:40:34 +0200

    Created TrackDto, TrackMapper, TrackController, TrackDataService, and TrackRepository

--- src/main/java/com/pluralsight/entertainmentmgr/track/controllers/TrackController.java
+++ src/main/java/com/pluralsight/entertainmentmgr/track/controllers/TrackController.java
@@ -1,43 +1,48 @@
 package com.pluralsight.entertainmentmgr.track.controllers;
 
 import com.pluralsight.entertainmentmgr.track.models.TrackDto;
-import com.pluralsight.entertainmentmgr.track.repositories.TrackRepository;
+import com.pluralsight.entertainmentmgr.track.services.TrackDataService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Log4j2
 public class TrackController {
 
-    private final TrackRepository trackRepository;
+    private final TrackDataService trackDataService;
 
     @GetMapping
     public ResponseEntity<List<TrackDto>> getAllTracks() {
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(trackDataService.findAllTracks());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TrackDto> getTrackById(@PathVariable Long id) {
-        return ResponseEntity.ok(null);
+        Optional<TrackDto> track = trackDataService.findTrackById(id);
+        return track.isPresent() ? ResponseEntity.ok(track.get()) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
     public ResponseEntity<TrackDto> createTrack(@RequestBody TrackDto trackDto) {
-        return ResponseEntity.ok(null);
+        TrackDto track = trackDataService.createTrack(trackDto);
+        return track != null ? ResponseEntity.ok(track) : ResponseEntity.badRequest().build();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<TrackDto> updateTrack(@RequestBody TrackDto trackDto, @PathVariable Long id) {
-        return ResponseEntity.ok(null);
+        TrackDto track = trackDataService.updateTrack(id, trackDto);
+        return track != null ? ResponseEntity.ok(track) : ResponseEntity.badRequest().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTrack(@PathVariable Long id) {
-        return ResponseEntity.ok(null);
+        trackDataService.deleteTrack(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/pluralsight/entertainmentmgr/track/models/TrackDto.java
+++ src/main/java/com/pluralsight/entertainmentmgr/track/models/TrackDto.java
@@ -1,12 +1,19 @@
 package com.pluralsight.entertainmentmgr.track.models;
 
-import com.pluralsight.entertainmentmgr.core.auditable.BaseDto;
-import lombok.*;
+import com.pluralsight.entertainmentmgr.core.auditable.models.BaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Getter
 @Setter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TrackDto extends BaseDto {
+    private String title;
+    private int durationSeconds;
+    private int yearReleased;
+    private int beatsPerMinute;
 }
