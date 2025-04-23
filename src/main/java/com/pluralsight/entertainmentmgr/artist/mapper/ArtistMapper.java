6848c1cd18aa3a3b7fab97fe8c0d061fbaca87a1
Carl OBeirne <carl.obeirne@expeditors.com>
2025-04-23 10:29:59 +0100

    Added mappers for entity/dto relationship

---
+++ src/main/java/com/pluralsight/entertainmentmgr/artist/mapper/ArtistMapper.java
@@ -0,0 +1,25 @@
+package com.pluralsight.entertainmentmgr.artist.mapper;
+
+import com.pluralsight.entertainmentmgr.artist.entities.Artist;
+import com.pluralsight.entertainmentmgr.artist.models.ArtistDto;
+import com.pluralsight.entertainmentmgr.core.mapper.BaseMapper;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface ArtistMapper extends BaseMapper<Artist, ArtistDto> {
+
+    @Override
+    ArtistDto toDTO(Artist entity);
+
+    @Override
+//    @Mapping(target = "tracks", ignore = true)
+    Artist toEntity(ArtistDto dto);
+
+    @Override
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+//    @Mapping(target = "tracks", ignore = true)
+    void updateEntityFromDto(ArtistDto dto, @MappingTarget Artist entity);
+}
