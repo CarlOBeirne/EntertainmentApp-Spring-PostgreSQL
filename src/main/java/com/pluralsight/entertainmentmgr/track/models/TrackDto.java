356785e103116bb2080a8a00b26df398cac6329a
Bassem Riachi <bassem.riachy@gmail.com>
2025-04-25 17:01:22 +0200

    Added null checks, InvalidTrackException. Implemented controller methods.

--- src/main/java/com/pluralsight/entertainmentmgr/track/models/TrackDto.java
+++ src/main/java/com/pluralsight/entertainmentmgr/track/models/TrackDto.java
@@ -1,10 +1,13 @@
 package com.pluralsight.entertainmentmgr.track.models;
 
+import com.pluralsight.entertainmentmgr.artist.models.ArtistDto;
 import com.pluralsight.entertainmentmgr.core.auditable.models.BaseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 
 @Getter
@@ -16,4 +19,5 @@ public class TrackDto extends BaseDto {
     private int durationSeconds;
     private int yearReleased;
     private int beatsPerMinute;
+    private Set<ArtistDto> artists;
 }
--- src/main/java/com/pluralsight/entertainmentmgr/track/services/TrackDataService.java
+++ src/main/java/com/pluralsight/entertainmentmgr/track/services/TrackDataService.java
@@ -1,15 +1,23 @@
 package com.pluralsight.entertainmentmgr.track.services;
 
+import com.pluralsight.entertainmentmgr.artist.entities.Artist;
+import com.pluralsight.entertainmentmgr.artist.models.ArtistDto;
+import com.pluralsight.entertainmentmgr.artist.repositories.ArtistRepository;
+import com.pluralsight.entertainmentmgr.core.exceptions.InvalidTrackException;
 import com.pluralsight.entertainmentmgr.track.entities.Track;
 import com.pluralsight.entertainmentmgr.track.mapper.TrackMapper;
 import com.pluralsight.entertainmentmgr.track.models.TrackDto;
 import com.pluralsight.entertainmentmgr.track.repositories.TrackRepository;
+import jakarta.transaction.Transactional;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,9 +27,11 @@ public class TrackDataService {
 
     private final TrackRepository trackRepository;
 
+    private final ArtistRepository artistRepository;
+
     private final TrackMapper trackMapper;
 
-    public Optional<TrackDto> findTrackById(Long trackId) {
+    public Optional<TrackDto> findTrackById(@NonNull Long trackId) {
         return trackRepository.findById(trackId)
                 .map(trackMapper::toDTO);
     }
@@ -32,22 +42,71 @@ public class TrackDataService {
                 .collect(Collectors.toList());
     }
 
-    public TrackDto createTrack(TrackDto trackDto) {
+    @Transactional
+    public TrackDto createTrack(@NonNull TrackDto trackDto) {
+        if (trackDto.getId() != null) {
+            throw new InvalidTrackException("Track id already exists");
+        }
         Track entity = trackMapper.toEntity(trackDto);
+        Set<Artist> artists = addArtist(trackDto);
+        entity.setArtists(artists);
         return trackMapper.toDTO(trackRepository.save(entity));
     }
 
+    @Transactional
     public TrackDto updateTrack(Long id, TrackDto trackDto) {
+        if (trackDto.getId() == null) {
+            throw new InvalidTrackException("Track ID does not exist.");
+        }
+        if (!trackDto.getId().equals(id)) {
+            throw new InvalidTrackException("Track id and id do not match.");
+        }
+
         Track entity = trackMapper.toEntity(trackDto);
         Optional<Track> existing = trackRepository.findById(id);
-        if (existing.isEmpty()) return null;
+        if (existing.isEmpty()) {
+            throw new InvalidTrackException("Track does not exist.");
+        }
         trackMapper.updateEntityFromDto(trackDto, existing.get());
         return trackMapper.toDTO(trackRepository.save(entity));
 
     }
 
+    @Transactional
     public void deleteTrack(Long trackId) {
         trackRepository.deleteById(trackId);
     }
 
+    public List<TrackDto> findTracksByTitle(@NonNull String title) {
+        return trackRepository.findAllByTitleIgnoreCase(title).stream()
+                .map(trackMapper::toDTO)
+                .toList();
+    }
+
+    private Set<Artist> addArtist(@NonNull TrackDto track) {
+        Set<Artist> artists = new HashSet<>();
+        if (track.getArtists() == null) {
+            throw new InvalidTrackException("No artist defined on track.");
+        }
+        for (ArtistDto artistDto : track.getArtists()) {
+            Optional<Artist> artist = artistRepository.findById(artistDto.getId());
+            if (artist.isPresent()) {
+                artists.add(artist.get());
+            }
+            else {
+                throw new InvalidTrackException("Artist does not exist.");
+            }
+        }
+        return artists;
+    }
+
+    @Transactional
+    public TrackDto removeArtist(Long trackId, Long artistId) {
+        Track track = trackRepository.findById(trackId).orElse(null);
+        Artist artist = artistRepository.findById(artistId).orElse(null);
+        if (track == null || artist == null) { return null; }
+
+        track.getArtists().remove(artist);
+        return trackMapper.toDTO(trackRepository.save(track));
+    }
 }
