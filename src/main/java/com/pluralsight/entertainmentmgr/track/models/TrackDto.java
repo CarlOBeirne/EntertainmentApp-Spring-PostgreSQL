e12c82222b79020a1fc31bbd5eb75151e77d569a
Bassem Riachi <bassem.riachy@gmail.com>
2025-04-28 17:28:57 +0200

    Added @SuperBuilder for TrackDto and GenreDto. Added a couple of @NonNull to TrackDataService. Wrote 15 tests on TrackDataService.

--- src/main/java/com/pluralsight/entertainmentmgr/track/models/TrackDto.java
+++ src/main/java/com/pluralsight/entertainmentmgr/track/models/TrackDto.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.Set;
 
@@ -15,6 +16,7 @@ import java.util.Set;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 public class TrackDto extends BaseDto {
     private String title;
     private int durationSeconds;
